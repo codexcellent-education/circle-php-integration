96de1e16a8402481e7ee9de30c63d8f06e7de86e
CodeXcellent Education <33389345+codexcellent-education@users.noreply.github.com>
2021-07-17 12:04:32 +0300

    Changes Week 3

---
+++ chargebacks/list-chargebacks.php
@@ -0,0 +1,33 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api-sandbox.circle.com/v1/chargebacks",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "Accept: application/json"
+  ],
+]);
+
+$access_token = "QVBJX0tFWTpmMjNlMDBlZDMwYmNkYzExOGE0N2RmYzk3ZmQ5OWIzMjo3N2Q0NWU5NTUwODM3MmYzZDM1NTg0ZGRhZmI4N2JlNg==";
+
+$cardheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_HTTPHEADER, $cardheader); //setting custom header
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
